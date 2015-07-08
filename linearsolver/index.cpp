3b2a50d1a5046f5f69e66ddf0fbdf87e6e332faa
Luohaothu <luohaothu@live.com>
2015-07-08 17:32:11 +0800

    finally clear all bugs! Remember % is unique for negetive num

--- linearsolver/index.cpp
+++ linearsolver/index.cpp
@@ -5,25 +5,44 @@ double xvalue(double x[dim1][dim2][dim3], int i, int j, int k, int p)
 {
 	switch (p + 1)
 	{
+	//case 1 : x(i, j, k)
 	case 1: return x[i][j][k]; break;
-	case 2: return x[(i - 1) % dim1][j][k]; break;
+	//case 2 : x(i - 1, j, k)
+	case 2: return x[(i - 1 + dim1) % dim1][j][k]; break;
+	//case 3 : x(i + 1, j, k)
 	case 3: return x[(i + 1) % dim1][j][k]; break;
-	case 4: return j == 0 ? x[(i - dim1 / 2) % dim1][j][k] : x[i][j - 1][k]; break;
-	case 5: return j == dim1 - 1 ? x[(i - dim1 / 2) % dim1][j][k] : x[i][j + 1][k]; break;
-	case 6: return j == dim1 - 1 ? 0 : x[(i + 1) % dim1][j + 1][k]; break;
-	case 7: return j == 0 ? 0 : x[(i + 1) % dim1][j - 1][k]; break;
-	case 8: return j == 0 ? 0 : x[(i - 1) % dim1][j - 1][k]; break;
-	case 9: return j == dim1 - 1 ? 0 : x[(i - 1) % dim1][j + 1][k]; break;
+	//case 4 : x(i, j - 1, k)
+	case 4: return j == 0 ? x[(i + dim1 / 2) % dim1][j][k] : x[i][j - 1][k]; break;
+	//case 5 : x(i, j + 1, k)
+	case 5: return j == dim2 - 1 ? x[(i + dim1 / 2) % dim1][j][k] : x[i][j + 1][k]; break;
+	//case 6 : x(i + 1, j + 1, k)
+	case 6: return j == dim2 - 1 ? x[(i + dim1 / 2 + 1) % dim1][j][k] : x[(i + 1) % dim1][j + 1][k]; break;
+	//case 7 : x(i + 1, j - 1, k)
+	case 7: return j == 0 ? x[(i + dim1 / 2 + 1) % dim1][j][k] : x[(i + 1) % dim1][j - 1][k]; break;
+	//case 8 : x(i - 1, j - 1, k)
+	case 8: return j == 0 ? x[(i + dim1 / 2 - 1) % dim1][j][k] : x[(i - 1 + dim1) % dim1][j - 1][k]; break;
+	//case 9 : x(i - 1, j + 1, k)
+	case 9: return j == dim2 - 1 ? x[(i + dim1 / 2 - 1) % dim1][j][k] : x[(i - 1 + dim1) % dim1][j + 1][k]; break;
+	//case 10: x(i, j, k - 1)
 	case 10:return k == 0 ? 0 : x[i][j][k - 1]; break;
-	case 11:return k == 0 ? 0 : x[(i - 1) % dim1][j][k - 1]; break;
+	//case 11: x(i - 1, j, k - 1)
+	case 11:return k == 0 ? 0 : x[(i - 1 + dim1) % dim1][j][k - 1]; break;
+	//case 12: x(i + 1, j, k - 1)
 	case 12:return k == 0 ? 0 : x[(i + 1) % dim1][j][k - 1]; break;
-	case 13:return k == 0 ? 0 : (j == 0 ? x[(i - dim1 / 2) % dim1][j][k - 1] : x[i][j - 1][k - 1]); break;
-	case 14:return k == 0 ? 0 : (j == dim1 - 1 ? x[(i - dim1 / 2) % dim1][j][k] : x[i][j + 1][k - 1]); break;
-	case 15:return k == dim1 - 1 ? 0 : x[i][j][k + 1]; break;
-	case 16:return k == dim1 - 1 ? 0 : x[(i - 1) % dim1][j][k + 1]; break;
-	case 17:return k == dim1 - 1 ? 0 : x[(i + 1) % dim1][j][k + 1]; break;
-	case 18:return k == dim1 - 1 ? 0 : (j == 0 ? x[(i - dim1 / 2) % dim1][j][k + 1] : x[i][j - 1][k + 1]); break;
-	case 19:return k == dim1 - 1 ? 0 : (j == dim1 - 1 ? x[(i - dim1 / 2) % dim1][j][k + 1] : x[i][j + 1][k + 1]); break;
+	//case 13: x(i, j - 1, k - 1)
+	case 13:return k == 0 ? 0 : (j == 0 ? x[(i + dim1 / 2) % dim1][j][k - 1] : x[i][j - 1][k - 1]); break;
+	//case 14: x(i, j + 1, k - 1)
+	case 14:return k == 0 ? 0 : (j == dim2 - 1 ? x[(i + dim1 / 2) % dim1][j][k - 1] : x[i][j + 1][k - 1]); break;
+	//case 15: x(i, j, k + 1)
+	case 15:return k == dim3 - 1 ? 0 : x[i][j][k + 1]; break;
+	//case 16: x(i - 1, j, k + 1)
+	case 16:return k == dim3 - 1 ? 0 : x[(i - 1 + dim1) % dim1][j][k + 1]; break;
+	//case 17: x(i + 1, j, k + 1)
+	case 17:return k == dim3 - 1 ? 0 : x[(i + 1) % dim1][j][k + 1]; break;
+	//case 18: x(i, j - 1, k + 1)
+	case 18:return k == dim3 - 1 ? 0 : (j == 0 ? x[(i + dim1 / 2) % dim1][j][k + 1] : x[i][j - 1][k + 1]); break;
+	//case 19: x(i, j + 1, k + 1)
+	case 19:return k == dim3 - 1 ? 0 : (j == dim2 - 1 ? x[(i + dim1 / 2) % dim1][j][k + 1] : x[i][j + 1][k + 1]); break;
 	default:return 0;
 		break;
 	}
